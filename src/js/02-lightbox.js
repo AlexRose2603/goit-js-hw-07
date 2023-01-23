83c22d11320d8161e0a0d143b95ff4b8f4e0b23f
AlexRose2603 <alexandra26korchak@gmail.com>
2023-01-23 17:54:40 +0200

    galleries

---
+++ src/js/02-lightbox.js
@@ -0,0 +1,46 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+
+function createImgItem(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+        <img 
+        class="gallery__image" 
+        src="${preview}" 
+        alt="${description}"/>
+        </a>
+        </div>`
+    )
+    .join("");
+}
+const preventRedirection = document.querySelector(".gallery");
+preventRedirection.addEventListener("click", onClick);
+function onClick(event) {
+  event.preventDefault();
+}
+const addGalleryMarkup = createImgItem(galleryItems);
+galleryEl.innerHTML = addGalleryMarkup;
+
+galleryEl.addEventListener("click", onImageClick);
+function onImageClick(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
+  galleryEl.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      close.simplelightbox();
+    }
+  });
+}
